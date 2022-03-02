84d0d02b204e44cf4e9d875e3900f4a308cbacbc
Habeeb Bello <habeeb@nestinvestco.com>
2022-03-02 12:20:48 +0100

    Model added || Database migration added and data seeding added

--- HotelListing.API/Startup.cs
+++ HotelListing.API/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,10 @@ namespace HotelListing.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
+
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll", builder =>
@@ -36,10 +41,13 @@ namespace HotelListing.API
                     .AllowAnyHeader());
                     
             });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing.API", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
